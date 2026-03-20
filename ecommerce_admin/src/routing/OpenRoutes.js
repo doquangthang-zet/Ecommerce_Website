b6ce4d0cfb75fa2fccf2425757361e194febbe9f
doquangthang-zet <s3891873@rmit.edu.vn>
2026-03-20 16:17:28 +0700

    image problem

--- ecommerce_admin/src/routing/OpenRoutes.js
+++ ecommerce_admin/src/routing/OpenRoutes.js
@@ -1,7 +1,10 @@
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const OpenRoutes = ({children}) => {
     const getTokenFromLocalStorage = localStorage.getItem("user");
+    const userState = useSelector(state => state?.auth?.user);
     
-    return !getTokenFromLocalStorage ? children : <Navigate to="/admin" replace={true} />;
+    
+    return !getTokenFromLocalStorage && userState?.role !== "admin" ? children : <Navigate to="/admin" replace={true} />;
 }
--- ecommerce_admin/src/routing/PrivateRoutes.js
+++ ecommerce_admin/src/routing/PrivateRoutes.js
@@ -1,8 +1,11 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const PrivateRoutes = ({children}) => {
     const location = useLocation();
+    const userState = useSelector(state => state?.auth?.user);
+
     const getTokenFromLocalStorage = localStorage.getItem("user");
-    
-    return getTokenFromLocalStorage ? children : (<Navigate to="/" replace state={{path: location.pathname}} />);
+
+    return getTokenFromLocalStorage && userState?.role === "admin" ? children : (<Navigate to="/.." replace state={{path: location.pathname}} />);
 };
