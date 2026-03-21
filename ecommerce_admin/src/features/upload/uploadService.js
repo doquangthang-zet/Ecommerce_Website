abf9aa221766a4f15d8e2612f70663ab7ff739be
doquangthang-zet <s3891873@rmit.edu.vn>
2026-03-21 11:13:50 +0700

    upload image problem

--- ecommerce_admin/src/features/upload/uploadService.js
+++ ecommerce_admin/src/features/upload/uploadService.js
@@ -2,16 +2,32 @@ import axios from "axios";
 import {baseUrl} from "../../utils/baseUrl";
 import { config } from "../../utils/axiosconfig";
 
+const getTokenFromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
 // Upload images to cloud
 const uploadImg = async (data) => {
-    const response = await axios.post(`${baseUrl}upload`, data, config);
+    const response = await axios.post(`${baseUrl}upload`, data, {
+      headers: {
+        Authorization: `Bearer ${
+          getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : ""
+        }`,
+      },
+    });
     return response.data;
 };
 
 const deleteImg = async (id) => {
     const response = await axios.delete(
       `${baseUrl}upload/deleteImage/${id}`,
-      config
+      {
+        headers: {
+          Authorization: `Bearer ${
+            getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : ""
+          }`,
+        },
+      }
     );
     return response.data;
   };
